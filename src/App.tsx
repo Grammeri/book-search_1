bdeff642df0fd26d631eff268ed0350e14b52a65
Dmitry Nikolayev <reacttsxgrammer@gmail.com>
2023-09-08 21:32:54 -0500

    fix: problem with start

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
+import SearchBar from './components/SearchBar/SearchBar';
+import CategoryFilter from './components/CategoryFilter/CategoryFilter';
+import SortFilter from './components/SortFilter/SortFilter';
+import BookCard from './components/BookCard/BookCard';
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState('all');
+  const [selectedSort, setSelectedSort] = useState('relevance');
+
+  const categories = [
+    'all',
+    'art',
+    'biography',
+    'computers',
+    'history',
+    'medical',
+    'poetry',
+  ];
+  const sortOptions = ['relevance', 'newest'];
+
+  // Dummy data for demonstration purposes
+  const books = [
+    {
+      title: "Harry Potter and the Sorcerer's Stone",
+      image: 'path_to_image.jpg',
+      category: 'fiction',
+      authors: ['J.K. Rowling'],
+    },
+    // ... More books for demonstration
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header">
+        <h1>Search for books</h1>
+        <SearchBar />
+        <div className="filters">
+          <CategoryFilter
+            categories={categories}
+            selectedCategory={selectedCategory}
+            onCategoryChange={setSelectedCategory}
+          />
+          <SortFilter
+            options={sortOptions}
+            selectedOption={selectedSort}
+            onOptionChange={setSelectedSort}
+          />
+        </div>
       </header>
+
+      <div className="book-list">
+        {books.map((book) => (
+          <BookCard key={book.title} {...book} />
+        ))}
+      </div>
     </div>
   );
 }
